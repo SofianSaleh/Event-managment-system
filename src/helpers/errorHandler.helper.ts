4bac168bc2ce9dfbaaec2d8554f92f114a147020
SofianSaleh <sofiane.saleh11@gmail.com>
2020-07-04 22:58:49 +0200

    validating

--- src/helpers/errorHandler.helper.ts
+++ src/helpers/errorHandler.helper.ts
@@ -1,3 +1,16 @@
+import { ValidationError } from "yup";
+
+export const formatYupErrors = async (err: ValidationError) => {
+  const errors: Array<{ path: string; msg: string }> = [];
+  err.inner.forEach((e) => {
+    errors.push({
+      path: e.path,
+      msg: e.message,
+    });
+  });
+  return errors;
+};
+
 export const responseFormatter = (
   success: boolean,
   msg: string,
