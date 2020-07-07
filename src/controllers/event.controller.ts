4529c9818740357b78cf41c7bccd0d9010d4180c
SofianSaleh <sofiane.saleh11@gmail.com>
2020-07-07 12:40:22 +0200

    :zap: done the boiler plate for the event controller

--- src/controllers/event.controller.ts
+++ src/controllers/event.controller.ts
@@ -1,7 +1,13 @@
 import Event from "../db/models/Event.model";
 
 class EventController {
-  public async getUser() {}
+  public async createEvent() {}
+  public async updateEvent() {}
+  public async deleteEvent() {}
+  public async getEvent() {}
+  public async getAllEvents() {}
+  public async getEventsBasedOnAUser() {}
+  public async getEventsInAnArea() {}
 }
 
 export default new EventController();
