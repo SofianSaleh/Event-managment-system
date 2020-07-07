b92a8b2d53dd4496b80efd01cb45fd0c38fbeef7
SofianSaleh <sofiane.saleh11@gmail.com>
2020-07-07 12:54:34 +0200

    :white_check_mark: done the graphql schema

--- src/controllers/event.controller.ts
+++ src/controllers/event.controller.ts
@@ -4,8 +4,10 @@ class EventController {
   public async createEvent() {}
   public async updateEvent() {}
   public async deleteEvent() {}
-  public async getEvent() {}
+  public async getEventById() {}
+  public async getEventTitle() {}
   public async getAllEvents() {}
+  public async addComment() {}
   public async getEventsBasedOnAUser() {}
   public async getEventsInAnArea() {}
 }
--- src/schemas/event.schema.ts
+++ src/schemas/event.schema.ts
@@ -1,1 +1,54 @@
-export default ``;
+import { gql } from "apollo-server-express";
+
+export default gql`
+  type Event {
+    id: String!
+    title: String!
+    description: String!
+    location: String!
+    date: String!
+    comments: Comment
+    location: [Float!]
+  }
+
+  type Query {
+    getEventById(id: string!): QueryResponse!
+    getEventByTitle(title:string!): GetMultipleEvents!
+    getAllEvents: GetMultipleEvents!
+    getEventsBasedOnAUser(username?:string, id: string): GetMultipleEvents!
+    getEventsInAnArea(): GetMultipleEvents!
+  }
+
+  type QueryResponse {
+      success: Boolean!
+      event: Event
+      errors: [Error!]
+  }
+
+  type GetMultipleEvents {
+      success: Boolean!
+      events: [Event!]
+      errors: [Error!]
+  }
+
+  type MutationResponse {
+      success: Boolean!
+      events: [Event!]
+      errors: [Error!]
+  }
+
+  input UpdateInput {
+    title: String
+    description: String
+    location: String
+    date: String
+    location: [Float!]
+  }
+
+  type Mutation {
+      createEvent(title: String!, description: String!, date: String!, location: [Float!]): MutationResponse!
+      updateEvent(updateObj: UpdateInput!): MutationResponse!
+      deleteEvent(eventId: String!): MutationResponse!
+      addComment(comment: string!): MutationResponse!
+  }
+`;
