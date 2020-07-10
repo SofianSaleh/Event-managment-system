3e26c79ae7feea442d620e2c4d338f1e37a658cf
SofianSaleh <sofiane.saleh11@gmail.com>
2020-07-10 05:24:31 +0200

    :wrench: fixing comments

--- src/controllers/comments.controller.ts
+++ src/controllers/comments.controller.ts
@@ -1,6 +1,7 @@
 import Comment from "../db/models/Comment.model";
 import userController from "./user.Controller";
 import eventController from "./event.controller";
+import chalk from "chalk";
 
 class CommentController {
   public async addComment(eventId: string, comment: string, userId: string) {
@@ -29,9 +30,9 @@ class CommentController {
     user_id: string
   ) {
     try {
-      const { success, errors, event } = await eventController.getEvent(
+      const { success, errors, event } = (await eventController.getEvent(
         event_id
-      );
+      )) as any;
       if (!success) return { success, errors };
 
       const user = await userController.getUser({ _id: user_id });
@@ -46,10 +47,19 @@ class CommentController {
           ],
         };
 
-      // event?.comments.filter(comment => comment.id !== comment_id);
-      // const deletedComment = await Comment.findByIdAndDelete(comment_id);
+      for (const [i, comment] of event?.comments.entries()) {
+        if (comment._id === comment_id) {
+          if (comment.user === user_id) {
+            event?.comments.splice(i, 1);
+          }
+        }
+      }
+      // event?.comments.filter(comment => comment.id !== comment_id && comment.user !==);
+      const deletedComment = await Comment.findByIdAndDelete(comment_id);
+      await event.save();
 
       console.log(comment_id, event?.comments);
+      console.log(chalk.green(deletedComment));
       return { success: true, event };
     } catch (e) {
       throw e;
